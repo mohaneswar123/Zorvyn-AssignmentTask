b9de1b1f3553079c89c998725bd692a0f89c0c47
mohaneswar123 <163732824+mohaneswar123@users.noreply.github.com>
2026-04-03 23:41:39 +0530

    Add GET method 'test' in AuthController that returns 'testinggggg'

--- src/main/java/com/finance/sdp/controller/AuthController.java
+++ src/main/java/com/finance/sdp/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.finance.sdp.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,5 +46,10 @@ public class AuthController {
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
         LoginResponse response = userService.loginUser(request.getEmail(), request.getPassword());
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("testinggggg");
     }
 }
